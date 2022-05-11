7543648d3731a3e0bf2891cf72ba022e8bbbb8f1
Eduardo Geraldini <eduardopr.geraldini@gmail.com>
2022-05-11 13:48:16 -0300

    initial commit

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "c35bab43a80145",
+    pass: "97cd67573063e6"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Eduardo Geraldini <eduardopr.geraldini@gmail.com>',
+      subject,
+      html: body,
+    });
+  }
+}
